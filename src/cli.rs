8ddb3c2bfb25b448967ca7fb332c67f47f329c84
Milo Auguste III <miloaugusteiii@outlook.com>
2022-04-15 10:48:35 -0400

    Adjustment

--- src/cli.rs
+++ src/cli.rs
@@ -33,6 +33,7 @@ pub enum Commands {
         #[clap(short, long, help = "Output File Path")]
         outpath: Option<String>,
     },
+    #[clap(name = "GET", about = "Download website", long_about = None, alias="get")]
     SiteDownload{
         url: String,
         #[clap(short, long, help = "Output directory for Site files. Will create if it doesn't exist.")]
